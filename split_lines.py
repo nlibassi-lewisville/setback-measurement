df3a702125f6a8a31218e278fdf8824b98fe352a
Nick Libassi <nlibassi@cityoflewisville.com>
2025-01-29 16:39:49 -0600

    begin splitting lines using angle formed by groups of three consecutive points along lines

--- split_lines.py
+++ split_lines.py
@@ -1,4 +1,5 @@
 import os
+import math
 import time
 import arcpy
 from shared import set_environment
@@ -10,7 +11,7 @@ def line_to_points(input_fc, output_fc):
     arcpy.management.FeatureVerticesToPoints(input_fc, output_fc, "ALL")
 
 
-def get_lines_with_x_points(input_fc, x):
+def get_lines_with_more_than_x_points(input_fc, x):
     """Get a list of line OBJECTIDs with more than x vertices."""
     print("Entered get_lines_with_x_points()...")
     line_oids = []
@@ -19,6 +20,100 @@ def get_lines_with_x_points(input_fc, x):
             if len(row[1].getPart(0)) > x:
                 line_oids.append(row[0])
     return line_oids
+
+
+def calculate_angle_from_points(start, end):
+    """
+    Calculate the angle (bearing) between first and last points of a line geometry in degrees, accounting for bidirectional lines.
+    :param start: The geometry object of the starting point.
+    :param end: The geometry object of the ending point.
+    :return: Angle in degrees (0-360).
+    """
+    start_geom = start.getPart()
+    end_geom = end.getPart()
+    dx = end_geom.X - start_geom.X
+    dy = end_geom.Y - start_geom.Y
+    angle = math.degrees(math.atan2(dy, dx))
+    # Normalize to 0-360 degrees
+    angle = angle % 360
+    # Normalize the angle to the range 0-180 (to account for bidirectional lines)
+    if angle > 180:
+        angle -= 180
+    return angle
+
+
+def get_points_for_splitting(input_point_fc, line_oids, angle_threshold):
+    """
+    Create a feature class of points at which lines will be split.
+    :param input_point_fc - string: Input point feature class (created from line feature class) that has a field called 'parcel_line_OID'
+    :param line_oids - list of int: List of line OBJECTIDs to process
+    :param angle_threshold - float: Threshold (in degrees) beyond which points will be used for splitting lines
+
+    TODO - remove if unused
+    :param output_point_fc - string: Output point feature class holding points at which lines will be split
+    """
+    print("Entered get_points_for_splitting()...")
+    oids_of_split_points = []
+    for oid in line_oids:
+        #arcpy.management.SelectLayerByAttribute(input_point_fc, "NEW_SELECTION", f"parcel_line_OID = {oid}")
+        # should be working only with selected point features from here forward but doesn't seem to be the case
+        feature_layer = "input_points_on_single_line"
+        arcpy.management.MakeFeatureLayer(input_point_fc, feature_layer, f"parcel_line_OID = {oid}")
+        #with arcpy.da.SearchCursor(input_point_fc, ["OBJECTID", "SHAPE@"], sql_clause=(None, 'ORDER BY OBJECTID ASC')) as cursor:
+        with arcpy.da.SearchCursor(feature_layer, ["OBJECTID", "SHAPE@"], sql_clause=(None, 'ORDER BY OBJECTID ASC')) as cursor:
+            for row in cursor:
+                previous_geom_1 = None
+                previous_geom_2 = None
+                for row in cursor:
+                    if not previous_geom_1:
+                        previous_geom_1 = row[1]
+                        continue
+                    elif not previous_geom_2:
+                        previous_geom_2 = row[1]
+                        continue
+                    else:
+                        oid = row[0]
+                        angle_1 = calculate_angle_from_points(previous_geom_1, previous_geom_2)
+                        angle_2 = calculate_angle_from_points(previous_geom_2, row[1])
+                        angle = abs(angle_2 - angle_1)
+                        #print(f"OID's: {oid-2}, {oid-1}, {oid}. Angle: {angle}")
+                        if angle > angle_threshold:
+                            # append OID of 2nd point in the 3-point sequence
+                            oids_of_split_points.append(oid - 1)
+                        elif angle < angle_threshold and angle > angle_threshold - 5:
+                            print(f"Angle: {angle} between points with OID {oid-2}, {oid-1}, and {oid} is within 5 degrees of threshold.")
+                        previous_geom_1 = previous_geom_2
+                        previous_geom_2 = row[1]
+
+    # same as below
+    #query_string = f"OBJECTID IN ({', '.join(map(str, oids_of_split_points))})"
+    query_string = f"OBJECTID IN ({', '.join(str(oid) for oid in oids_of_split_points)})"
+    print(f"length of oids_of_split_points: {len(oids_of_split_points)}")
+    #arcpy.management.SelectLayerByAttribute(input_point_fc, "NEW_SELECTION", query_string)
+    output_feature_layer = "output_points_for_splitting"
+    arcpy.management.MakeFeatureLayer(input_point_fc, output_feature_layer, query_string)
+    #arcpy.management.SelectLayerByAttribute(input_point_fc, "NEW_SELECTION", query_string)
+    output_point_fc = f"points_for_splitting_angle_threshold_{angle_threshold}"
+    arcpy.management.CopyFeatures(output_feature_layer, output_point_fc)
+
+
+    #spatial_reference = arcpy.Describe(input_fc).spatialReference
+    #out_path = os.getenv("FEATURE_DATASET")
+    #arcpy.CreateFeatureclass_management(
+    #    out_path=out_path,
+    #    out_name=output_fc_name,
+    #    geometry_type="POINT",
+    #    spatial_reference=spatial_reference
+    #)
+    #output_fc = os.path.join(out_path, output_fc_name)
+    #with arcpy.da.SearchCursor(input_fc, ["OBJECTID", "SHAPE@"]) as search_cursor, \
+    #     arcpy.da.InsertCursor(output_fc, ["SHAPE@"]) as insert_cursor:
+    #    for row in search_cursor:
+    #        if row[0] in line_oids:
+    #            line = row[1]
+    #            part = line.getPart(0)
+    #            for point in part:
+    #                insert_cursor.insertRow([point])
 
 
 def get_clustered_points(input_fc, line_oids, output_fc_name, cluster_vertex_min, cluster_distance_max):
@@ -183,13 +278,18 @@ def split_lines(input_fc, points_fc, output_fc, search_radius=250):
 
 def run(min_vertices=2):
     start_time = time.time()
+    print(f"Starting process of splitting lines at {time.ctime(start_time)}")
     set_environment()
     # Define input and output feature classes
     # parcel_lines_in_zones_r_th_otmu_li_ao has already been split at corners
-    #input_fc_name = "parcel_lines_in_zones_r_th_otmu_li_ao"
-    input_fc_name = "parcel_lines_from_polygons_TEST"
+    #input_line_fc_name = "parcel_lines_in_zones_r_th_otmu_li_ao"
+    input_line_fc_name = "parcel_lines_from_polygons_TEST"
+    # all points from parcel lines
+    input_point_fc_name = "points_from_parcel_lines_from_polygons_TEST"
+    #subset of points from parcel lines for testing
+    #input_point_fc_name = "subset_points_from_parcel_lines_from_polygons_TEST"
     feature_dataset = os.getenv("FEATURE_DATASET")
-    input_fc = os.path.join(feature_dataset, input_fc_name)
+    input_fc = os.path.join(feature_dataset, input_line_fc_name)
     output_midpoints_fc_name = "midpoints_and_corners_20250128"
     output_midpoints_fc = os.path.join(feature_dataset, output_midpoints_fc_name)
     output_split_lines_fc = os.path.join(feature_dataset, "split_parcel_lines_in_zones_r_th_otmu_li_ao_20250128")
@@ -199,20 +299,22 @@ def run(min_vertices=2):
     # TODO check for existence first and uncomment if needed
     #arcpy.management.Delete(output_split_lines_fc)
     #arcpy.management.Delete(output_midpoints_fc)
-    #arcpy.management.Delete(temp_points_fc)
+    arcpy.management.Delete(temp_points_fc)
 
     # Step 1: Convert lines to points
     line_to_points(input_fc, temp_points_fc)
 
     # Step 2: Get lines with more than x points
-    line_oids = get_lines_with_x_points(input_fc, min_vertices)
+    line_oids = get_lines_with_more_than_x_points(input_fc, min_vertices)
 
     # Step 3: Calculate and save midpoints and corners
     #get_midpoints_and_clusters(input_fc, line_oids, output_midpoints_fc_name, 7, 40)
-    get_clustered_points(input_fc, line_oids, output_midpoints_fc_name, 6, 50)
+    #get_clustered_points(input_fc, line_oids, output_midpoints_fc_name, 6, 50)
+
+    get_points_for_splitting(input_point_fc_name, line_oids, 15)
 
     # Step 4: Split lines at midpoints
-    split_lines(input_fc, output_midpoints_fc, output_split_lines_fc, 250)
+    #split_lines(input_fc, output_midpoints_fc, output_split_lines_fc, 250)
 
     # Cleanup
     #arcpy.management.Delete(temp_points_fc)
