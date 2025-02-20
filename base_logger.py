e4e43cc7eb25a4e6fb9ce03c73ea2aac58b039d8
Nick Libassi <nlibassi@cityoflewisville.com>
2025-02-20 16:24:56 -0600

    base_logger.py

--- base_logger.py
+++ base_logger.py
@@ -1,6 +1,7 @@
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Creating an object
 logger = logging.getLogger()
